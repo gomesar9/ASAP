0a0baf023cf21e5acee36bd2ebbbdea9528f610c
GomesAR <agomes@lasca.ic.unicamp.br>
2017-08-06 15:36:05 -0300

    Concertando o #define DEBUG 1. E utilizando função debug() pra outros prints.

--- EventMonitor/EventMonitor/src/config.h
+++ EventMonitor/EventMonitor/src/config.h
@@ -6,7 +6,7 @@
 #include "dbg/debug.h"
 
 
-#define DEBUG /* define if debug info will be printed */
+#define DEBUG 1/* define if debug info will be printed */
 #define DRIVER_NAME "[EVENT-MONITOR]" /* define the driver name printed when debugging */
 #define DRIVERNAME L"\\Device\\EventMonitor" /* driver name for windows subsystem */
 #define DOSDRIVERNAME L"\\DosDevices\\EventMonitor" /* driver name for ~DOS~ subsystem */
