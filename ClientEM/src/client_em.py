1069965efba8dac1de2c63c659a2ce42b2736be1
ra191071 <gomes.bcc@gmail.com>
2018-03-08 10:48:20 -0300

    client plotando estático, com mini-shell e configurações on the fly

--- ClientEM/src/client_em.py
+++ ClientEM/src/client_em.py
@@ -3,6 +3,11 @@
 import win32file as w
 import configparser
 import time
+# plot
+import matplotlib.pyplot as plt
+from numpy import arange
+
+
 
 
 class ClientEM():
@@ -11,6 +16,7 @@ class ClientEM():
 
 
     def __init__(self, exec_type="simulation", debug=False):
+        print("Client for EventMonitor ({ver}).".format(ver=ClientEM._ClientEM__VERSION))
         self.__exec_type = exec_type
 
         self.__em = None
@@ -46,14 +52,14 @@ class ClientEM():
 
         try:
             self.__em = w.CreateFile(
-		self.__driver_fpath, 
-		_desiredAccess,
-		_shareMode,
-		_attributes,
-		_creationDisposition,
-		_flagsAndAttributes,
-		_hTemplateFile
-		)
+                self.__driver_fpath, 
+                _desiredAccess,
+                _shareMode,
+                _attributes,
+                _creationDisposition,
+                _flagsAndAttributes,
+                _hTemplateFile
+            )
         except Exception as e:
             print("[!] '__connect_to_event_monitor()' Error: " + str(e))
             return False
@@ -77,6 +83,37 @@ class ClientEM():
 
         return True
 
+    
+    def set_config(self, n_reads=None, interval=None):
+        if n_reads:
+            if type(n_reads) == int:
+                self.__n_reads = n_reads
+            else:
+                print("[!] Invalid 'n_reads' type. Accepted: int")
+        if interval:
+            if type(interval) == int or type(interval) == float:
+                self.__interval = interval
+            else:
+                print("[!] Invalid 'interval' type. Accepted: int, float")
+
+
+    def plot_data(self, data_y, data_x=None):
+        if not data_x:
+            data_x = arange(0.0, len(data_y) * self.__interval, self.__interval)
+
+        fig, ax = plt.subplots()
+        ax.plot(data_x, data_y)
+
+        ax.set(
+                xlabel="time (s)",
+                ylabel="count",
+                title="[{}] plot test".format(self.__exec_type)
+                )
+        ax.grid()
+
+        #fig.savefig("last_run.png")
+        plt.show()
+
 
     def start(self):
         if not self.__setup_ok:
@@ -96,7 +133,8 @@ class ClientEM():
 
             time.sleep(self.__interval)
         
-        # TODO plot graphic
+        # Plot data
+        self.plot_data(data)
 
         self.last_run_data = data
         # TODO save data
@@ -104,10 +142,95 @@ class ClientEM():
         return True
  
 
+    def __del__(self):
+        w.CloseHandle(self.__em)
+        
+
+class CEMShell():
+    """Shell for ClientEM"""
+
+
+    def __init__(self, config_file="client.cfg", debug=False):
+        self.debug = debug
+        self.config_file = config_file
+
+        self.__cem = ClientEM(debug=self.debug)
+        self.__cmds = {
+                "run": self.__run,
+                "config": self.__config,
+                "dbg": self.__set_debug,
+                "?": self.__help,
+                }
+
+
+    def __run(self):
+        self.__cem.start()
+
+
+    def __config(self):
+        try:
+            _n_reads = input("Insert n_reads.\n>")
+            _interval = input("Insert interval.\n>")
+
+            self.__cem.set_config(
+                    n_reads = int( _n_reads ),
+                    interval = float( _interval )
+                    )
+
+        except Exception as e:
+            print("[!] Error: " + str(e))
+
+
+    def __set_debug(self):
+        try:
+            _opt = input("CEMS_DEBUG({}). Set debug (on/off):\n>".format(self.debug))
+            if _opt == "on":
+                self.debug = True
+            if _opt == "off":
+                self.debug = False
+            
+            _opt = input("ClientEM_DEBUG({}). Set debug (on/off):\n>".format(self.__cem.debug))
+            if _opt == "on":
+                self.__cem.debug = True
+            if _opt == "off":
+                self.__cem.debug = False
+        except Exception as e:
+            print("[!] Error: " + str(e))
+
+
+    def __help(self):
+        print("Commands: run, config, dbg")
+
+
+    def __exit(self):
+        pass
+
+
+    def menu(self):
+        print("[ClientEM Shell]")
+        self.__help()
+        print("? for help")
+        try:
+            cmd = None
+            while (cmd != "exit"):
+                cmd = input("[CEMS]> ")
+
+                if cmd in self.__cmds:
+                    if self.debug:
+                        print("[D] cmd: '{}'".format(self.__cmds[cmd]))
+                    self.__cmds[cmd]()
+        except Exception as e:
+            print("[!] Error: " + str(e) )
+
+        self.__exit()
+
+        
 if __name__ == "__main__":
-     print("Client for EventMonitor ({ver}).".format(ver=ClientEM._ClientEM__VERSION))
-     print("Use:\n c = ClientEM(debug=True)")
-     print(" c.start()")
-     print("# Results in c.last_run_data")
+     #print("Client for EventMonitor ({ver}).".format(ver=ClientEM._ClientEM__VERSION))
+     #print("Use:\n c = ClientEM(debug=True)")
+     #print(" c.start()")
+     #print("# Results in c.last_run_data")
+     cems = CEMShell(debug=True)
+     cems.menu()
 
 
