a535ee9e2d1ce109bce1d733477c36e7af5f45f4
GomesAR <agomes@lasca.ic.unicamp.br>
2018-04-24 19:34:51 -0300

    Enabling do PEBS. Código para start: 0000, código para stop: 0200

--- EventMonitor/EventMonitor/src/ems/EMS.c
+++ EventMonitor/EventMonitor/src/ems/EMS.c
@@ -8,6 +8,13 @@
 #ifdef SIMULATION
 ULONG emsSeed = 1337;
 #endif
+HANDLE thandle;
+BOOLEAN PEBS_ACTIVE = FALSE;
+
+// PEBS base and buffer
+PTDS_BASE DS_BASE;
+PTPEBS_BUFFER PEBS_BUFFER;
+
 
 NTSTATUS _unpack(const PANSI_STRING cmd, PEM_CMD emCmd) {
 	NTSTATUS st = STATUS_SUCCESS;
@@ -56,7 +63,7 @@ NTSTATUS execute(const PANSI_STRING cmd) {
 	}
 
 	if (emCmd.Type == EM_CMD_SET) {
-		debug("EM_SET_EVENT detected.");
+		debug("EM_SET detected.");
 
 		switch (emCmd.Event) {
 		case EM_EVT_CACHE_SS:
@@ -70,8 +77,58 @@ NTSTATUS execute(const PANSI_STRING cmd) {
 			debug(dbgMsg);
 			break;
 		}
+	} else if (emCmd.Type == EM_CMD_START) {
+		debug("EM_START detected.");
+		if (PEBS_ACTIVE) {
+			// TODO: Return better code error
+			return STATUS_RM_ALREADY_STARTED;
+		}
+
+		// Install hook before start Thread
+		hook_handler();
+
+		switch (emCmd.Event) {
+		case EM_STCFG_CORE0:
+			debug("Activating PEBS in core 0.");
+			st = PsCreateSystemThread(&thandle, GENERIC_ALL, NULL, NULL, NULL, StarterThread, (VOID*) 0);
+
+			if (NT_SUCCESS(st)) {
+				PEBS_ACTIVE = TRUE;
+			} else {
+				PEBS_ACTIVE = FALSE;
+			}
+			break;
+		case EM_STCFG_CORE1:
+			debug("Activating PEBS in core 1. (FUTURE)");
+			break;
+		default:
+			sprintf(dbgMsg, "%d is not a valid Start Configuration.", emCmd.Event);
+			debug(dbgMsg);
+			break;
+		}
 	}
-	else {
+	else if (emCmd.Type == EM_CMD_STOP) {
+		if (PEBS_ACTIVE == FALSE) {
+			// TODO: Return better code error
+			return STATUS_FAIL_CHECK;
+		}
+
+		switch (emCmd.Event) {
+		case EM_STCFG_CORE0:
+			debug("Deactivating PEBS in core 0.");
+			st = PsCreateSystemThread(&thandle, GENERIC_ALL, NULL, NULL, NULL, StopperThread, (VOID*)0);
+
+			// uninstall hook after stop Thread
+			unhook_handler();
+			PEBS_ACTIVE = FALSE;
+			break;
+		default:
+			sprintf(dbgMsg, "%d is not a valid Stop Configuration.", emCmd.Event);
+			debug(dbgMsg);
+			break;
+		}
+
+	} else {
 		// Error
 		sprintf(dbgMsg, "%d is not a valid CMD.", emCmd.Type);
 		debug(dbgMsg);
@@ -103,4 +160,158 @@ NTSTATUS sample(PANSI_STRING emBfr) {
 	}
 
 	return STATUS_SUCCESS;
+}
+
+
+/********************************************
+* Interrupt Routine
+*/
+VOID PMI(__in struct _KINTERRUPT *Interrupt, __in PVOID ServiceContext) {
+	UNREFERENCED_PARAMETER(Interrupt);
+	UNREFERENCED_PARAMETER(ServiceContext);
+
+	LARGE_INTEGER pa;
+	UINT32* APIC;
+	//LONG intPID;
+	// Disable PEBS
+	__writemsr(MSR_IA32_PEBS_ENABLE, DISABLE_PEBS);
+	__writemsr(MSR_IA32_GLOBAL_CTRL, DISABLE_PEBS);
+
+	//intPID = (LONG)PsGetCurrentProcessId();
+
+	// --+-- Clear APIC flag --+--
+	pa.QuadPart = PERF_COUNTER_APIC;
+	/*
+	NTKERNELAPI PVOID MmMapIoSpace(
+		PHYSICAL_ADDRESS	PhysicalAddress,
+		SIZE_T				NumberOfBytes,
+		MEMORY_CACHING_TYPE	CacheType
+	);
+	*/
+	APIC = (UINT32*)MmMapIoSpace(pa, sizeof(UINT32), MmNonCached);
+	*APIC = ORIGINAL_APIC_VALUE;
+	MmUnmapIoSpace(APIC, sizeof(UINT32));
+
+	// TODO: PROCESS
+	debug("Interrupt routine reached");
+
+	// TODO: Re-enable PEBS in near future
+
+}
+
+typedef union ACUCAR {
+	PVOID V;
+	VOID (*F) (__in struct _KINTERRUPT *, __in PVOID);
+} ACUCAR;
+
+// Hook Handles
+ACUCAR blah;
+PVOID restore_hook = NULL;
+
+/*
+	Install PMI
+*/
+VOID hook_handler() {
+	NTSTATUS st;
+	/*
+	NTSTATUS HalSetSystemInformation(
+		_In_	HAL_QUERY_INFORMATION_CLASS	InformationClass,
+		_In_	ULONG						BufferSize,
+		_Out_	PVOID						Buffer
+	);
+	*/
+	blah.F = PMI;
+	PVOID perfmon_hook = blah.V;
+
+	st = HalSetSystemInformation(
+		HalProfileSourceInterruptHandler,
+		sizeof(PVOID*),
+		&perfmon_hook
+	);
+	char msg[126];
+	sprintf(msg, "HOOK_HANDLER: ST:%X. F:%p", st, blah.V);
+	debug(msg);
+}
+
+/*
+	Uninstall PMI
+*/
+VOID unhook_handler() {
+	NTSTATUS st;
+	st = HalSetSystemInformation(
+		HalProfileSourceInterruptHandler,
+		sizeof(PVOID*),
+		&restore_hook
+	);
+}
+
+
+/********************************************
+ * THREAD FUNCTIONS
+*/
+VOID thread_attach_to_core(uintptr_t id) {
+	// High level magic, do not touch, do not look, just skip. What are you waiting for?
+	KAFFINITY mask;
+#pragma warning( disable : 4305 )
+#pragma warning( disable : 4334 )
+	mask = 1 << id;
+	KeSetSystemAffinityThread(mask);
+}
+
+VOID fill_ds_with_buffer(PTDS_BASE ds_base, PTPEBS_BUFFER pebs_buffer) {
+	ds_base->PEBS_BUFFER_BASE = pebs_buffer;
+	ds_base->PEBS_INDEX = pebs_buffer;
+	ds_base->PEBS_MAXIMUM = pebs_buffer+1;
+	ds_base->PEBS_THRESHOLD = pebs_buffer;
+}
+
+VOID StarterThread(_In_ PVOID StartContext) {
+	uintptr_t core;
+	// Get core information (number)
+	core = (uintptr_t)StartContext;
+
+	debug("Thread Iniciada");
+
+	// Attach thread to core
+	thread_attach_to_core(core);
+
+	// TODO: Allocate structs and buffers
+	DS_BASE = (PTDS_BASE)ExAllocatePoolWithTag(NonPagedPool, sizeof(TDS_BASE), 'DSB');
+	PEBS_BUFFER = (PTPEBS_BUFFER)ExAllocatePoolWithTag(NonPagedPool, sizeof(PTPEBS_BUFFER), 'PBF');
+	fill_ds_with_buffer(DS_BASE, PEBS_BUFFER);
+
+	// --+-- Enable mechanism --+--
+	// Disable PEBS to set up
+	__writemsr(MSR_IA32_GLOBAL_CTRL, DISABLE_PEBS);
+	__writemsr(MSR_IA32_PERFCTR0, PERIOD);
+	// Enable events
+	__writemsr(MSR_IA32_EVNTSEL0, PEBS_EVENT | EVTSEL_EN | EVTSEL_USR | EVTSEL_INT);
+	// Enable PEBS
+	__writemsr(MSR_IA32_PEBS_ENABLE, ENABLE_PEBS);
+	__writemsr(MSR_IA32_GLOBAL_CTRL, ENABLE_PEBS);
+	
+	debug("PEBS setado");
+}
+
+VOID StopperThread(_In_ PVOID StartContext) {
+	//int core;
+	uintptr_t core;
+	core = (uintptr_t)StartContext;
+	debug("Stopping thread");
+	
+	// Attach to a given core
+	thread_attach_to_core(core);
+
+	// --+-- Disable PEBS for counter 0 --+--
+	__writemsr(MSR_IA32_PEBS_ENABLE, DISABLE_PEBS);
+	__writemsr(MSR_IA32_GLOBAL_CTRL, DISABLE_PEBS);
+
+	// Unhook PMI
+	unhook_handler();
+
+	// Free allocated memory
+	ExFreePoolWithTag(PEBS_BUFFER, 'PBF'); // Buffer
+	ExFreePoolWithTag(DS_BASE, 'DSB');	// Struct
+
+	debug("Thread stopped. Resources FREE");
 }
--- EventMonitor/EventMonitor/src/ems/EMS.h
+++ EventMonitor/EventMonitor/src/ems/EMS.h
@@ -1,12 +1,61 @@
 #pragma once
 #include "../config.h"
+#include <intrin.h>
+
+
+/* #################################################
+ BTS struct
+*/
+typedef struct st_BTSBUFFER {
+	UINT64 FROM, TO, MISC;
+}TBTS_BUFFER, *PTBTS_BUFFER;
+
+/*
+ PEBS struct
+*/
+typedef struct st_PEBSBUFFER {
+	UINT64
+		RFLAGS, RIP, RAX, RBX, RCX,
+		RDX,	RSI, RDI, RBP, RSP,
+		R8, R9, R10, R11, R12, R13, R14, R15;
+}TPEBS_BUFFER, *PTPEBS_BUFFER;
+
+typedef struct st_DSBASE {
+	// Pointers to BTS struct
+	PTBTS_BUFFER
+		BTS_BUFFER_BASE,
+		BTS_INDEX,
+		BTS_MAXIMUM,
+		BTS_THRESHOLD;
+
+	// Pointers to PEBS struct
+	PTPEBS_BUFFER
+		PEBS_BUFFER_BASE,
+		PEBS_INDEX,
+		PEBS_MAXIMUM,
+		PEBS_THRESHOLD;
+}TDS_BASE, *PTDS_BASE;
+/* #################################################
+ ###################################################
+*/
 
 /* Enums */
 typedef enum _EM_CMD_TYPE {
+	EM_CMD_START,
 	EM_CMD_SET,
+	EM_CMD_STOP,
 	EM_CMD_NULL
 }EM_CMDTYPE, *PEM_CMDTYPE;
 
+// Enum (options) for EM_CMD_START
+typedef enum _START_CFG {
+	EM_STCFG_CORE0,
+	EM_STCFG_CORE1,
+	EM_STCFG_CORE2,
+	EM_STCFG_CORE3
+}START_CFG, *PSTART_CFG;
+
+// Enum (options) for EM_CMD_SET
 typedef enum _EM_EVENT {
 	EM_EVT_CACHE_SS,
 	EM_EVT_BRANCH_SS,
@@ -32,3 +81,37 @@ NTSTATUS sample(PANSI_STRING info);
 //NTSTATUS bfr_destroy();
 
 #define EMS_CMD_MAX_LENGTH 4
+#define DISABLE_PEBS 0
+#define ENABLE_PEBS 1
+
+// Limit
+#define PERIOD 1
+
+#define EVTSEL_EN 1<<22
+#define EVTSEL_USR 1<<16
+#define EVTSEL_INT 1<<20
+
+#define MSR_IA32_PERFCTR0			0xC1
+#define MSR_IA32_EVNTSEL0			0x186
+#define MSR_IA32_GLOBAL_CTRL		0x38F
+#define MSR_IA32_GLOAL_STATUS		0x38E
+#define MSR_IA32_GLOBAL_OVF_CTRL	0x390
+#define MSR_IA32_PEBS_ENABLE		0x3F1
+
+#define ORIGINAL_APIC_VALUE			254
+
+#define PERF_COUNTER_APIC			0xFEE00340
+
+#define PEBS_EVENT 0x1c2
+
+/*
+ Functions
+*/
+
+VOID PMI(__in struct _KINTERRUPT *Interrupt, __in PVOID ServiceContext);
+VOID hook_handler();
+VOID unhook_handler();
+VOID thread_attach_to_core(uintptr_t id);
+VOID fill_ds_with_buffer(PTDS_BASE ds_base, PTPEBS_BUFFER pebs_buffer);
+VOID StarterThread(_In_ PVOID StartContext);
+VOID StopperThread(_In_ PVOID StartContext);
